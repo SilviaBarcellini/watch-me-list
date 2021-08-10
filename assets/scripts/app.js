f042d8748db1802a8c3e902fe2aa88e5c7f6d031
Your Name <barcellinisilvia@gmail.com>
2021-08-11 00:26:31 +0200

    ✨ add user input  validation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,16 @@
-const addMovieItem = document.getElementById("add-modal"); 
+const newForm = document.getElementById("add-modal"); 
 const startAddMovieBtn = document.querySelector("header button");
 const backDrop = document.getElementById("backdrop");
 const cancel = document.querySelector(".btn--passive"); 
+const add = cancel.nextElementSibling; 
+const userInputs = newForm.querySelectorAll("input"); //select all input 
 
 const backdropGetsGray = () => {
     backDrop.classList.toggle("visible"); 
 }
 
 const showForm = () => {
-    addMovieItem.classList.toggle("visible");
+    newForm.classList.toggle("visible");
     backdropGetsGray(); 
 }
 
@@ -16,10 +18,28 @@ const backDropClickHandler = () => {
     showForm()
 }
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
     showForm()
+}
+
+const addMovieHandler = () => {//map through inputs and isolate the values
+    const titleValue = userInputs[0].value; 
+    const imageUrlValue = userInputs[1].value;
+    const ratingValue = userInputs[2].value; 
+
+    if (
+        titleValue.trim() === "" ||
+        imageUrlValue.trim() === "" ||
+        ratingValue.trim() === "" ||
+        +ratingValue < 1 ||
+        +ratingValue > 5
+    ) {
+        alert("UH-OH! Something went wrong! Please make sure you enter valid values!!");
+        return; 
+    }
 }
 
 startAddMovieBtn.addEventListener("click", showForm); 
 backDrop.addEventListener("click", showForm);
-cancel.addEventListener("click", cancelAddMovie);
+cancel.addEventListener("click", cancelAddMovieHandler);
+add.addEventListener("click", addMovieHandler)
